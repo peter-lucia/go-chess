673cb553b7073ccc4de1dd0acd12c4b5dfe65368
Peter Lucia <peter-lucia@users.noreply.github.com>
2024-07-19 23:03:33 -0700

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
-echo "go build main.go"
-go build main.go
 
-# setup a chess library
+cd ../
+echo "Setting up ./engine"
 mkdir -p engine
 cd engine/
 go mod init github.com/peter-lucia/go-chess/engine
 go mod edit -replace github.com/peter-lucia/go-chess/engine=../engine
 
-cd ../
 
+cd ../
+echo "Setting up ./ui"
+mkdir -p ui
+go mod init github.com/peter-lucia/go-chess/ui
+go mod edit -replace github.com/peter-lucia/go-chess/ui=../ui
+
+cd ../
+echo "Setting up ./engine"
 mkdir -p runner
 cd runner/
 go mod init github.com/peter-lucia/go-chess/runner
 go get ../engine
+go get ../ui
 go mod tidy
 
 cd ../
+
+go work init ./engine ./ui ./runner
+
+go run ./runner
